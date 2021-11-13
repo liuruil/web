148f5d016860ec0812c6070621ec70f6ed6f9821
liuruil <2223015815@qq.com>
2021-11-13 22:14:32 +0800

    实现新的promise任务处理 第一步

--- js/promise/index.js
+++ js/promise/index.js
@@ -1,107 +1,57 @@
 // 手写promise
 const MyPromise = (function () {
-    const RESOLVED = 'resolved'
+    const FULFILLED = 'fulfilled'
     const REJECTED = 'rejected'
     const PENDING = 'pending'
-    const promiseValue = Symbol('promiseValue') //promise的值
-    const promiseStatus = Symbol('promiseStatus') //promise的状态
-    const changeStatus = Symbol('changeState') //改变状态的函数
-    const thenables = Symbol('thenables') //thenable队列
-    const catchables = Symbol('catchables') //catchable队列
-    const settleHandle = Symbol('settleHandle') //后续处理函数
-    return class MyPromise {
-
+    return class {
         /**
          * 
-         * @param {*} status 要变成的状态
-         * @param {*} value  promise改变的值
-         * @param {*} queue  状态对应的队列
-         * @returns 
-         */
-        [changeStatus](status, value, queue) {
-            if (this[promiseStatus] !== PENDING) {
-                return;
-            }
-            this[promiseValue] = value
-            this[promiseStatus] = status
-            //执行队列
-            queue.forEach(handel => handel(this[promiseValue]))
-        }
-
-        /**
-         * @description: 后续处理函数的通用方法
-         * @param {*} handle 处理函数
-         * @param {*} queue 相应队列
-         * @param {*} status 改变的状态
-         * @return {*}
-         */
-        [settleHandle](handle, queue, status) {
-            if (this[promiseStatus] === status) {
-                setTimeout(() => {
-                    handle(this[promiseValue])
-                }, 0)
-            } else {
-                queue.push(handle)
-            }
-        }
-
-
-        /**
-         * @param {*} executor  promise回调函数
-         * @return {*}
+         * @param {*} executor  任务执行器
          */
         constructor(executor) {
-            this[catchables] = [] //初始化catch队列
-            this[thenables] = [] //初始化then队列
-            this[promiseValue] = undefined
-            this[promiseStatus] = PENDING
-            const resolved = (data) => {
-                this[changeStatus](RESOLVED, data, this[thenables])
-            }
-            const rejected = (err) => {
-                this[changeStatus](REJECTED, err, this[catchables])
-            }
+            this._state = PENDING; // promise的状态
+            this._value = undefined; // promise的值
             try {
-                executor(resolved, rejected)
+                executor(this._resolved.bind(this), this._rejected.bind(this))
             } catch (error) {
-                rejected(error)
+                // 捕获错误进入任务失败处理
+                this._rejected(error)
             }
         }
 
         /**
-         * @description: then方法实现
-         * @param {*} thenable then的第一个参数
-         * @param {*} catchable then的第二个参数
-         * @return {*}
+         * 更改任务的状态和值
+         * @param {*} newState  新的状态
+         * @param {*} value  新的值
+         * @returns 
          */
-        then(thenable, catchable) {
-            this[settleHandle](thenable, this[thenables], RESOLVED)
-            this.catch(catchable)
-        }
-
-        /**
-         * @description: catch方法实现
-         * @param {*} catchable catch的第一个参数
-         * @return {*}
-         */
-        catch(catchable) {
-            if (typeof catchable !== 'function') {
+        _changeState(newState, value) {
+            if (this._state !== PENDING) {
                 return;
             }
-            this[settleHandle](catchable, this[catchables], REJECTED)
+            this._state = newState;
+            this._value = value;
+        }
+
+        /**
+         * 标记当前任务完成
+         * @param {*} data  成功的数据
+         */
+        _resolved(data) {
+            this._changeState(FULFILLED, data);
+        }
+
+        /**
+         * 标记当前任务失败
+         * @param {*} reason  失败的原因
+         */
+        _rejected(reason) {
+            this._changeState(REJECTED, reason);
         }
     }
 })()
 
 const pro = new MyPromise((resolved, rejected) => {
-    setTimeout(() => {
-        rejected(1)
-    }, 2000)
-})
-pro.then((data) => {
-    console.log(data)
-})
-pro.catch((data) => {
-    console.log('catch', data)
+    throw new Error('哈哈哈')
 })
 console.log(pro)
